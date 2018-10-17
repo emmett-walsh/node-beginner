d622f9a7e0ca34bc698a34d1f09a2ab232c5449b
Emmett Walsh <emmett.finbarr.walsh@gmail.com>
2018-10-17 10:30:02 +0100

    Introduce router.js
    All routes are handled the same, but we now have a seam to introduce routing logic.
    Also injected router.route() as a dependency via index.js

--- server.js
+++ server.js
@@ -1,15 +1,21 @@
-const http = require("http");
+var http = require("http");
+var url = require("url");
 
-function start() {
+function start(route) {
   function onRequest(request, response) {
-    console.log("Request received.");
-    response.writeHead(200, { "Content-Type": "text/plain"});
+    console.log("Request for " + pathname(request.url) + " received.");
+    route(pathname(request.url));
+    response.writeHead(200, { "Content-Type": "text/plain"} );
     response.write("Hello World");
     response.end();
   }
 
   http.createServer(onRequest).listen(3000);
   console.log("Server has started");
+}
+
+function pathname(unparsedUrl) {
+  return url.parse(unparsedUrl).pathname;
 }
 
 exports.start = start;
